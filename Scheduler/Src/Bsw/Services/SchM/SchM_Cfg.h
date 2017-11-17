ebe833c8d3058effb52952286b00acf5379ad6de
07_ZU_FA <uidn8311@contiwan.com>
2017-11-17 14:46:09 -0600

    update SchM_Cfg.h, SchM_Cfg.c

--- Scheduler/Src/Bsw/Services/SchM/SchM_Cfg.h
+++ Scheduler/Src/Bsw/Services/SchM/SchM_Cfg.h
@@ -1,16 +1,60 @@
+/*============================================================================*/
+/*                        I BS SOFTWARE GROUP                                 */
+/*============================================================================*/
+/*                        OBJECT SPECIFICATION                                */
+/*============================================================================*/
+/*!
+ * $Source: SchM_Cfg.h
+ * $Revision: 1
+ * $Author: Rodrigo Mortera
+ * $Date: 17/NOV/2017
+ */
+/*============================================================================*/
+/* DESCRIPTION :                                                              */
+/** \file
+    short description in one sentence end with dot.
+    detailed
+    multiline
+    description of the file
+*/
+/*============================================================================*/
+/* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
+/* AUTOMOTIVE GROUP, Interior Division, Body and Security                     */
+/* ALL RIGHTS RESERVED                                                        */
+/*                                                                            */
+/* The reproduction, transmission, or use of this document or its content is  */
+/* not permitted without express written authority. Offenders will be liable  */
+/* for damages.                                                               */
+/* All rights, including rights created by patent grant or registration of a  */
+/* utility model or design, are reserved.                                     */
+/*                                                                            */
+/*============================================================================*/
+/*============================================================================*/
+/*                    REUSE HISTORY - taken over from                         */
+/*============================================================================*/
+/*  Author           |        Version     |           DESCRIPTION             */
+/*----------------------------------------------------------------------------*/
+/*  Rodrigo Mortera   |      1             |  Use the template and add the code*/
+/*============================================================================*/
+/*                               OBJECT HISTORY                               */
+/*============================================================================*/
 /*
+ *
  * SchM_Cfg.h
  *
  *  Created on: 15/11/2017
  *      Author: uid87753
  */
-
+ /* ============================================================================*/
 #ifndef BSW_SERVICES_SCHM_SCHM_CFG_H_
 #define BSW_SERVICES_SCHM_SCHM_CFG_H_
 
+/* Includes */
 #include "Std_Types.h"
+/*============================================================================*/
 
 
+/* Constants and types */
 typedef uint8_t SchM_TaskOffsetType;
 
 typedef enum{
@@ -56,5 +100,13 @@ typedef struct {
 }SchM_ConfigType;
 
 const SchM_ConfigType SchedulerConfig;
+/*============================================================================*/
 
+
+/* Exported Variables */
+/*============================================================================*/
+
+
+/* Exported functions prototypes */
+/*============================================================================*/
 #endif /* BSW_SERVICES_SCHM_SCHM_CFG_H_ */
